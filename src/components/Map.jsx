5f6530fb81552c99451fc73cb283215b04d88e90
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2022-11-14 00:59:44 -0500

    added and styled sidebar

---
+++ src/components/Map.jsx
@@ -0,0 +1,4 @@
+import "./Map.css";
+export default function Map() {
+  return <div id="map"></div>;
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,62 @@
+import "./Sidebar.css";
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img src="../../public/logo.png" alt="Logo" className="logo" />
+
+      <ul className="workouts">
+        <form className="form hidden">
+          <div className="form__row">
+            <label className="form__label">Type</label>
+            <select className="form__input form__input--type">
+              <option value="running">Running</option>
+              <option value="cycling">Cycling</option>
+            </select>
+          </div>
+          <div className="form__row">
+            <label className="form__label">Distance</label>
+            <input
+              className="form__input form__input--distance"
+              placeholder="km"
+            />
+          </div>
+          <div className="form__row">
+            <label className="form__label">Duration</label>
+            <input
+              className="form__input form__input--duration"
+              placeholder="min"
+            />
+          </div>
+
+          <div className="form__row">
+            <label className="form__label">Cadence</label>
+            <input
+              className="form__input form__input--cadence"
+              placeholder="step/min"
+            />
+          </div>
+          <div className="form__row form__row--hidden">
+            <label className="form__label">Elev Gain</label>
+            <input
+              className="form__input form__input--elevation"
+              placeholder="meters"
+            />
+          </div>
+          <button className="form__btn">OK</button>
+        </form>
+      </ul>
+
+      <p className="copyright">
+        &copy; Copyright by{" "}
+        <a
+          className="twitter-link"
+          target="_blank"
+          href="https://twitter.com/eyuelmulugeta21"
+        >
+          Eyuel Mulugeta.
+        </a>{" "}
+        Design by Jonas Schmedtmann.
+      </p>
+    </div>
+  );
+}
